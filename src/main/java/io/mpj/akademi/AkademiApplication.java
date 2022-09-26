374498ae7122b723ccd492a45903dc3d73ccaa2e
empeje <11813607+empeje@users.noreply.github.com>
2022-09-26 08:27:01 +0200

    Add springboot

---
+++ src/main/java/io/mpj/akademi/AkademiApplication.java
@@ -0,0 +1,13 @@
+package io.mpj.akademi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkademiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkademiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mpj/akademi/AkademiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mpj.akademi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkademiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
